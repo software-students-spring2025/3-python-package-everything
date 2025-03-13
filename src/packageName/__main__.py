441017a71f50ec824282a237cca4a7aa10fc5efb
ivapark <cmpark1656@gmail.com>
2025-03-12 23:29:21 -0400

    feeling.py updated

--- src/packageName/__main__.py
+++ src/packageName/__main__.py
@@ -1,6 +1,7 @@
 # import packageName.faces as faces
 from faces import replace_with_faces
 from stories import face_story
+from feelings import ask_user_feeling
 
 
 def main():
@@ -15,6 +16,11 @@ def main():
     print(face_story(3))
     print("\nLOOK BEHIND YOU...\n")
     print("BOO!\n")
+
+    # Ask the user how they feel and show a related face emoji
+    print("\n Feeling Check! \n")
+    print("\n How are you feeling today? (happy, sad, mad, soso): \n")
+    print(ask_user_feeling("happy")) 
     
 if __name__ == "__main__":
     # run the main function
