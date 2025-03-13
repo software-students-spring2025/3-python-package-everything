def69c9c0f3ba21ab6e31e17207c17969aa13e8a
jenniferyuuu <jy4172@nyu.edu>
2025-03-13 15:01:19 -0400

    include encoder function in main

--- src/facedbook/__main__.py
+++ src/facedbook/__main__.py
@@ -2,7 +2,7 @@
 from faces import replace_with_faces
 from stories import face_story
 from feelings import ask_user_feeling
-
+from encoder import face_encoder
 
 def main():
     # line = faces.replace_with_faces()
@@ -21,6 +21,10 @@ def main():
     print("\n Feeling Check! \n")
     print("\n How are you feeling today? (happy, sad, mad, soso): \n")
     print(ask_user_feeling("happy")) 
+
+    # testing face_encoder
+    print("\nPrinting happy faces...")
+    print(face_encoder("happy", 8))
     
 if __name__ == "__main__":
     # run the main function
