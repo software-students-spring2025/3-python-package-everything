e63890ac173cad29abd4de1192608a28041877a2
radishsoups <theobsidianheart@gmail.com>
2025-03-13 20:30:32 -0400

    Changing testing message in main

--- src/facedbook/__main__.py
+++ src/facedbook/__main__.py
@@ -5,10 +5,9 @@ from feelings import ask_user_feeling
 from encoder import face_encoder
 
 def main():
-    # line = faces.replace_with_faces()
     print(
         replace_with_faces(
-            'All team members must have visibly contributed to the code using their own git & GitHub accounts in order to claim that they contributed to the project.'
+            'I want to go to bed.'
         ))
 
     # testing for face_story
