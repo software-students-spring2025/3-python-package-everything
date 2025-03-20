bead54fb3cd2b188dd9be8a1ab44634a80b52d10
ChrisimKim <ck3388@nyu.edu>
2025-03-20 14:26:24 -0400

    modified __main__.py

--- src/facedbook/__main__.py
+++ src/facedbook/__main__.py
@@ -7,16 +7,17 @@ import facedbook.encoder as encoder
 
 def main():
     # testing replace_with_faces
+    print("\nReplacing with faces!")
     print(faces.replace_with_faces("This project is so cool and fun to use!"))
 
     # testing face_story
-    print("\nspooky story... \n")
+    print("\nSpooky story... \n")
     print(stories.face_story(3))
     print("\nLOOK BEHIND YOU...\n")
     print("BOO!\n")
 
     # Ask the user how they feel and show a related face emoji
-    print("\n Feeling Check! \n")
+    print("\nFeeling Check! \n")
     print("How are you feeling today? (happy, sad, mad, soso, excited, tired, nervous, love, surprised, confused, scared, bored): \n")
     print(feelings.ask_user_feeling("happy"))
 
